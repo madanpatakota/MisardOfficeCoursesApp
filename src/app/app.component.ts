4f5a3f8891cf849226762d168ba1fa2e778ff664
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 23:05:13 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
+import { Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Misard-Courses-App';
+  constructor(private router: Router) {
+  this.router.events.subscribe((event) => {
+    if (event instanceof NavigationEnd) {
+      AOS.init({ once: true }); // once: true = runs only once per element
+    }
+  });
+}
 }
